19b8e46976493b0463c221f91ae0f2be38321ea3
John Patrick <jpattrick538@gmail.com>
2023-01-15 16:22:52 +0300

    Simon Challenge Day 1
    Added animation and sound to buttons

--- Challanges/Simon Game Challenge Starting Files/index.js
+++ Challanges/Simon Game Challenge Starting Files/index.js
@@ -3,13 +3,63 @@
 var randomPattern = [];
 var playerInput = [];
 
+var rpLength = randomPattern.length;
+
 
            //game start
+//Animation functions
+
+function displayRestart(){
+    $(' #green').addClass('pressed');
+    var audio = new Audio('sounds/blue.mp3');
+    audio.play();     
+    setTimeout(function(){ $('#green').removeClass('pressed')}, 100);
+
+}
+function displayGreen(){
+    $(' #green').addClass('pressed');
+    var audio = new Audio('sounds/blue.mp3');
+    audio.play();     
+    setTimeout(function(){ $('#green').removeClass('pressed')}, 100);
+
+}
+function displayRed(){
+    $(' #red').addClass('pressed');
+    var audio = new Audio('sounds/blue.mp3');
+    audio.play();     
+    setTimeout(function(){ $('#red').removeClass('pressed')}, 100);
+
+}
+function displayYellow(){
+    $(' #yellow').addClass('pressed');
+    var audio = new Audio('sounds/blue.mp3');
+    audio.play();     
+    setTimeout(function(){ $('#yellow').removeClass('pressed')}, 100);
+
+}
+function displayBlue(){
+    $(' #blue').addClass('pressed');
+    var audio = new Audio('sounds/blue.mp3');
+    audio.play();     
+    setTimeout(function(){ $('#blue').removeClass('pressed')}, 100);
+
+}
+function displayGameOver(){
+    $('body').addClass('game-over');
+    var audio = new Audio('sounds/wrong.mp3');
+    audio.play();     
+    $('#level-title').text('Level 1');
+
+}
+
+//End of Animation functions
 
 
 //Level 1
-
-
+for(l= 0 ; rpLength < l ; l++) { startGame();
+    
+}
+function startGame() {
 $('body').on("keypress", function(){
     
     var randomNumber = Math.floor((Math.random() * 4) + 1);
@@ -18,34 +68,24 @@ $('body').on("keypress", function(){
     switch (randomNumber){
         case 1:
             randomPattern.push(randomNumber);
-            $(' #green').addClass('pressed');
-            var audio = new Audio('sounds/green.mp3');
-            audio.play();    
-            setTimeout(function(){ $(' #green').removeClass('pressed')}, 100);
-            break;
+            displayGreen();
             
+            break;
+
         case 2:
-                randomPattern.push(randomNumber);
-                $(' #red').addClass('pressed');
-                var audio = new Audio('sounds/red.mp3');
-                audio.play();      
-                setTimeout(function(){ $(' #red').removeClass('pressed')}, 100);
-                break;
+            randomPattern.push(randomNumber);
+            displayRed();
+
+            break;
 
         case 3:
             randomPattern.push(randomNumber);
-            $('#yellow').addClass('pressed');
-            var audio = new Audio('sounds/yellow.mp3');
-            audio.play();       
-            setTimeout(function(){ $(' #yellow').removeClass('pressed')}, 100);
+            displayYellow();
             break;  
 
         case 4:
             randomPattern.push(randomNumber);
-            $(' #blue').addClass('pressed');
-            var audio = new Audio('sounds/blue.mp3');
-            audio.play();     
-            setTimeout(function(){ $('#blue').removeClass('pressed')}, 100);
+            displayBlue();
 
         break;
         default:
@@ -54,59 +94,100 @@ $('body').on("keypress", function(){
 
     }
 })
+}
 // End of Level 1//
 
 //Start of Level 2//
 
 //color Values
 
-var redButton = 1;
-var redButton = 1;
-var redButton = 1;
+var greenButton = 1;
+var redButton = 2;
+var yellowButton = 3;
 var blueButton = 4;
 
-$('body').on("click", function(){
+
+//Click Inputs
+
+$('.btn').on("click", function(){
+    var clickedButton = this.id;
     
-    var clickedKey = this.innerHTML;
-    $('#level-title').text('Level 1');
-    
-    switch (randomNumber){
-        case 1:
-            randomPattern.push(randomNumber);
-            $(' #green').addClass('pressed');
-            var audio = new Audio('sounds/green.mp3');
-            audio.play();    
-            setTimeout(function(){ $(' #green').removeClass('pressed')}, 100);
+    switch (clickedButton){
+
+        case 'green':
+            displayGreen();
+            playerInput.push(1);
+            randomPattern.push(1);
+            addToRandomPattern();
+            readCurrentPattern();
             break;
             
-        case 2:
-                randomPattern.push(randomNumber);
-                $(' #red').addClass('pressed');
-                var audio = new Audio('sounds/red.mp3');
-                audio.play();      
-                setTimeout(function(){ $(' #red').removeClass('pressed')}, 100);
-                break;
-
-        case 3:
-            randomPattern.push(randomNumber);
-            $('#yellow').addClass('pressed');
-            var audio = new Audio('sounds/yellow.mp3');
-            audio.play();       
-            setTimeout(function(){ $(' #yellow').removeClass('pressed')}, 100);
+        case 'red':
+            displayRed();
+            playerInput.push(2)
+            randomPattern.push(2);
+            addToRandomPattern();
+            readCurrentPattern();
+                
+            break;
+                
+        case 'yellow':
+            displayYellow();
+            playerInput.push(3);
+            randomPattern.push(3);
+            addToRandomPattern();
+            readCurrentPattern();
+            
             break;  
-
-        case 4:
-            randomPattern.push(randomNumber);
-            $(' #blue').addClass('pressed');
-            var audio = new Audio('sounds/blue.mp3');
-            audio.play();     
-            setTimeout(function(){ $('#blue').removeClass('pressed')}, 100);
-
-        break;
+            
+        case 'blue':
+            displayBlue();
+            playerInput.push(4);
+            addToRandomPattern();
+            readCurrentPattern();
+                
+                
+            break;
         default:
-            alert(randomNumber);
-        break
+            alert(5);
+            break
+        }
 
     }
-})
 
+)
+
+//Game Engine
+//var randomPattern = [];
+//var playerInput = [];
+
+
+function addToRandomPattern(){
+    var randomAddition = Math.floor((Math.random() * 4) + 1);
+    randomPattern.push(randomAddition);
+
+}
+
+function readCurrentPattern(){
+    
+    for (i=0; i < rpLength; i++){
+    var light = randomPattern[i];
+        if (light === 1){
+            displayGreen();
+           // 
+        }
+        else if(light === 2){
+            displayRed();
+           // 
+        }
+        else if(light === 3){
+            displayYellow();
+            //
+        }
+        else if(light === 4){
+            displayBlue();
+           // randomPattern.push(2);
+        }
+        
+    }
+}
