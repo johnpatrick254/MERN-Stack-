c22c961646945a3b30abd493edb598070bdaf4e8
John Patrick <jpattrick538@gmail.com>
2023-02-15 16:49:44 +0300

    completed the to-do-list

--- Challanges/to-do-list/ap.js
+++ Challanges/to-do-list/ap.js
@@ -1,32 +1,47 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const logic = require("logic.js")
 app.use(bodyParser.urlencoded({extended:true}))
 app.set("view engine", 'ejs')
 
 app.use(express.static("public"))
 
 var items = []
-
+var workList = []
 
 app.get("/", function(req,res){
-   var options = { weekday: 'long', month: 'long', day: 'numeric' };
-   var date = new Date()
- var today = date.toLocaleDateString("en-US", options)
- var currentDay = date.getDay()
+ let today = logic.day()
+ console.log(today);
+ var currentDay = logic.date()
+ console.log(currentDay);
  if(currentDay === 6 || currentDay === 0){
-  res.render("list", {kindofDay:today, newItem:items})
+  res.render("list", {listTitle:today, newItem:items})
   }else{
-   res.render("list", {kindofDay:today , newItem:items
+   res.render("list", {listTitle:today , newItem:items
   })
  }
 
 })
 app.post("/", function(req,res){
    var input = req.body.new_item;
-  items.push(input)
-  res.redirect('/')
+   if(req.body.button === "Work"){
+    workList.push(input)
+    res.redirect('/work')
+   } else{
+     items.push(input)
+     res.redirect('/')
+   }
+})
+
+app.get("/work", function(req,res){
+  res.render('list', {listTitle: "Work List", newItem:workList})
+
+})
+app.get("/about", function(req,res){
+  res.render('about' )
+
 })
 app.listen(3000, function(){
-    console.log("Server started");
+    console.log("Server started on Port 3000");
 })
