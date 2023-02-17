cd8580e4dbdbcb7a0d04defe11cc814cf0cfaec6
John Patrick <jpattrick538@gmail.com>
2023-02-17 13:33:56 +0300

    completed blogsite challenge

---
+++ Challanges/Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,31 @@
+//img 1
+
+var randomNumber_1 =Math.floor(Math.random() * 6) + 1;
+
+var diceImg_1 = document.querySelectorAll('img')[0];
+
+var randomDice_1 = './images/dice' + randomNumber_1 + '.png';
+
+diceImg_1.setAttribute('src', randomDice_1);
+
+//img2
+
+var randomNumber_2 = Math.floor(Math.random() * 6) + 1;
+
+var diceImg_2 = document.querySelectorAll('img')[1];
+
+var randomDice_2 = './images/dice' + randomNumber_2 + '.png';
+
+diceImg_2.setAttribute('src', randomDice_2);
+
+//winner selection
+
+if (randomNumber_1 > randomNumber_2) {
+    document.querySelector('h1').innerHTML = "🚩Player 1 Won"
+}
+else if (randomNumber_2 > randomNumber_1){
+    document.querySelector('h1').innerHTML = "🚩Player 2 Won"
+}
+else {
+    document.querySelector('h1').innerHTML = "Draw!"
+}
--- Challanges/to-do-list/ap.js
+++ Challanges/to-do-list/ap.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const logic = require("logic.js")
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.set("view engine", 'ejs')
 
 app.use(express.static("public"))
@@ -10,38 +12,46 @@ app.use(express.static("public"))
 var items = []
 var workList = []
 
-app.get("/", function(req,res){
- let today = logic.day()
- console.log(today);
- var currentDay = logic.date()
- console.log(currentDay);
- if(currentDay === 6 || currentDay === 0){
-  res.render("list", {listTitle:today, newItem:items})
-  }else{
-   res.render("list", {listTitle:today , newItem:items
-  })
- }
+app.get("/", function (req, res) {
+  let today = logic.day()
+  console.log(today);
+  var currentDay = logic.date()
+  console.log(currentDay);
+  if (currentDay === 6 || currentDay === 0) {
+    res.render("list", {
+      listTitle: today,
+      newItem: items
+    })
+  } else {
+    res.render("list", {
+      listTitle: today,
+      newItem: items
+    })
+  }
 
 })
-app.post("/", function(req,res){
-   var input = req.body.new_item;
-   if(req.body.button === "Work"){
+app.post("/", function (req, res) {
+  var input = req.body.new_item;
+  if (req.body.button === "Work") {
     workList.push(input)
     res.redirect('/work')
-   } else{
-     items.push(input)
-     res.redirect('/')
-   }
+  } else {
+    items.push(input)
+    res.redirect('/')
+  }
 })
 
-app.get("/work", function(req,res){
-  res.render('list', {listTitle: "Work List", newItem:workList})
+app.get("/work", function (req, res) {
+  res.render('list', {
+    listTitle: "Work List",
+    newItem: workList
+  })
 
 })
-app.get("/about", function(req,res){
-  res.render('about' )
+app.get("/about", function (req, res) {
+  res.render('about')
 
 })
-app.listen(3000, function(){
-    console.log("Server started on Port 3000");
+app.listen(3000, function () {
+  console.log("Server started on Port 3000");
 })
